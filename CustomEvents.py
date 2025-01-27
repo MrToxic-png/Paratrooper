ec64d37c895e9f1a21da1cffe45461b48da4dd85
ME <alexo.oozdvg@gmail.com>
2025-01-27 18:41:02 +0300

    +Wave

--- CustomEvents.py
+++ CustomEvents.py
@@ -7,3 +7,7 @@ assert init_pygame.get_pygame_init()
 UPDATE_ANIMATION = pygame.USEREVENT + 1
 
 pygame.time.set_timer(UPDATE_ANIMATION, 200)
+
+SPAWN_NEW_AVIATION = pygame.USEREVENT + 2
+
+pygame.time.set_timer(SPAWN_NEW_AVIATION, 400)
--- Wave.py
+++ Wave.py
@@ -4,7 +4,56 @@
 -Информация о всех вертолетах, каждый из которых в свою очередь будет хранить в себе информацию о том,
 когда и где он сбросит парашютистов
 -Информация о всех самолетах (самолеты тоже можно будет разделить на мини-волны) и о том, сбросят они бомбу или нет"""
+from random import randint
+
+import CustomEvents
+from Sprites import HelicopterLeft, HelicopterRight
 
 
 class EnemyWave:
-    pass
+
+    max_helicopters = randint(8, 10)
+    max_jet = randint(3, 5)
+
+    def __init__(self, mini_waves, next_waves=0):
+        self.mini_waves = mini_waves
+        self.next_waves = next_waves
+        self.helicopter_count = 0
+        self.jet_count = 0
+        self.jet_or_helicopter = True
+
+    def update(self, *args, **kwargs):
+        if self.mini_waves == 0:
+            if self.next_waves != 0:
+                EnemyWave(self.next_waves)
+        if args:
+            event = args[0]
+            if event.type == CustomEvents.SPAWN_NEW_AVIATION:
+                if self.jet_or_helicopter and self.helicopter_count <= self.max_helicopters:
+                    self.spawn_helicopter()
+                    self.helicopter_count += 1
+                else:
+                    if self.helicopter_count > self.max_helicopters:
+                        self.jet_or_helicopter = False
+                        self.helicopter_count = 0
+                    if self.jet_count <= self.max_jet:
+                        self.spawn_jet()
+                        self.jet_count += 1
+                    else:
+                        self.jet_or_helicopter = True
+                        self.jet_count = 0
+
+
+
+
+    def spawn_helicopter(self):
+        if randint(0, 1) == 0:
+            HelicopterLeft()
+        else:
+            HelicopterRight()
+
+    def spawn_jet(self):
+        if randint(0, 1) == 0:
+            HelicopterLeft()
+        else:
+            HelicopterRight()
